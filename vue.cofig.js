4a00b039b579482e3b5c65e395dc8808b9b9108d
pigZhu <520xzh1314@gmail.com>
2020-06-09 08:42:11 +0800

    malltabbar

---
+++ vue.cofig.js
@@ -0,0 +1,47 @@
+// const path = require('path')
+// const resolve = dir => path.join(__dirname, dir)
+//
+// module.exports = {
+//   chainWebpack: config => {
+//     // 设置快捷路径， @ 表示 'src' ，components 表示 'src/components'
+//     config.resolve.alias
+//       .set('@', resolve('src'))
+//       .set('assets', resolve('src/assets'))
+//       .set('components', resolve('src/components'))
+//       .set('views', resolve('src/views'))
+//   },
+// }
+
+
+// const path = require('path')
+// // const path = require('path');//引入path模块
+// function resolve(dir){
+//   return path.join(__dirname,dir)//path.join(__dirname)设置绝对路径
+// }
+// module.exports={
+//   chainWebpack:(config)=>{
+//     config.resolve.alias
+//       .set('@',resolve('./src'))
+//       .set('components',resolve('./src/components'))
+//       .set('views',resolve('./src/views'))
+//       .set('assets',resolve('./src/assets'))
+//     //set第一个参数：设置的别名，第二个参数：设置的路径
+//
+//   }
+// }
+
+
+
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//         'assets': '@/assets',
+//         'common': '@/common',
+//         'components': '@/components',
+//         'network': '@/network',
+//         'views': '@/views',
+//       }
+//     }
+//   }
+// }
