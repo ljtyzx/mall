fc139f28fb6c73f18bb691a307614306d3d605e5
pigZhu <520xzh1314@gmail.com>
2020-06-26 15:44:09 +0800

    '1'

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
       viewportHeight: 667, //视窗的高度，对应是我们设计稿的高度（也可以不配置）
       unitPrecision: 5, //指定'px'转换为视窗单位值的小数位数(很多时候无法整除)
       viewportUnit: 'vw', //指定需要转换成的视窗单位，建议使用vw
-      selectorBlackList: ['ignore','tab-bar','tab-bar-item'], //指定不需要转化换的类，后面再讲
+      selectorBlackList: ['ignore','tab-bar','tab-bar-item','bottom-bar'], //指定不需要转化换的类，后面再讲
       minPixelValue: 1, // 小于或等于'1px'不转换为视窗单位
       mediaQuery: false //允许在媒体查询中转换'px'
     },
